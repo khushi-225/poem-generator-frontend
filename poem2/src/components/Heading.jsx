c9c2d01f5b735dc97b4e061b698bd23c93447113
Khushi Maheshwari <114518961+khushi-225@users.noreply.github.com>
2024-08-29 14:38:53 +0530

    Delete poem2 directory

--- poem2/src/components/Heading.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Heading.module.css";
-
-const Heading = () => {
-  return <h1 className={styles.heading}>AI Poetry Generator</h1>;
-};
-
-export default Heading;
